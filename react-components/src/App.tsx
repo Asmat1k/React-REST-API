642bff12fc143a7f25600111d58c65fc6b7071cb
Asmat1k <dobrovti@mail.ru>
2023-10-24 14:14:43 +0300

    refactor: made App a class component and add some type for API

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,14 +1,17 @@
 import Search from './components/search';
 import List from './components/list';
 import './styles/nullstyles.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <>
-      <Search />
-      <List />
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Search />
+        <List />
+      </>
+    );
+  }
 }
 
 export default App;
--- react-components/src/components/search/index.tsx
+++ react-components/src/components/search/index.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 import styles from './search.module.scss';
 
 class Search extends React.Component {
+  inputState = {
+    value: '',
+  };
+
+  handleInputEvent(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
       <section className={styles.search}>
-        <input className={styles.input} type="text" />
-        <button className={styles.button}>Search</button>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Type StarWars character..."
+          maxLength={20}
+          onChange={(e) => this.handleInputEvent(e)}
+        />
+        <button className={styles.button} onClick={() => console.log('click')}>
+          Search
+        </button>
       </section>
     );
   }
