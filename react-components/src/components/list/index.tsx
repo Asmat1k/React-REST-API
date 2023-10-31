7c31cdc1b0f9c489d5308a257fb13a201534a7c9
Asmat1k <dobrovti@mail.ru>
2023-10-31 17:20:10 +0300

    feat: change List to functional component

--- react-components/src/components/list/index.tsx
+++ react-components/src/components/list/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 import styles from './list.module.scss';
 
-import { ApiProps } from '../../types/types';
+import { ApiItem, ApiProps } from '../../types/types';
 import Item from '../item';
 
 interface ListProps {
@@ -10,41 +8,34 @@ interface ListProps {
   isLoading: boolean;
 }
 
-class List extends React.Component<ListProps> {
-  constructor(props: ListProps) {
-    super(props);
-  }
-
-  render() {
-    const { isLoading } = this.props;
-    if (isLoading) {
-      return (
-        <div className={styles.loading_wrapper}>
-          <div className={styles.loading}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+function List({ data, isLoading }: ListProps) {
+  if (isLoading) {
+    return (
+      <div className={styles.loading_wrapper}>
+        <div className={styles.loading}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-      );
-    }
-
-    const { results } = this.props.data;
-
-    if (results && results.length > 0) {
-      return (
-        <section className={styles.data}>
-          <ul className={styles.list}>
-            {results.map((character, index) => (
-              <Item data={character} key={index} myKey={index} />
-            ))}
-          </ul>
-        </section>
-      );
-    }
-    return <div className={styles.empty}>Nothing was found...</div>;
+      </div>
+    );
   }
+
+  const { results } = data;
+
+  if (results && results.length > 0) {
+    return (
+      <section className={styles.data}>
+        <ul className={styles.list}>
+          {results.map((character: ApiItem, index: number) => (
+            <Item data={character} key={index} myKey={index} />
+          ))}
+        </ul>
+      </section>
+    );
+  }
+  return <div className={styles.empty}>Nothing was found...</div>;
 }
 
 export default List;
