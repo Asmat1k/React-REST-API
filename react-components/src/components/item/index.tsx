537317efc8ea528bfea5ce8fcf79215327d9c208
Asmat1k <dobrovti@mail.ru>
2023-10-31 17:21:48 +0300

    feat: change Item to functional component

--- react-components/src/components/item/index.tsx
+++ react-components/src/components/item/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './item.module.scss';
 import { ApiItem } from '../../types/types';
 
@@ -7,23 +6,16 @@ interface ItemProps {
   myKey: number;
 }
 
-class Item extends React.Component<ItemProps> {
-  constructor(props: ItemProps) {
-    super(props);
-  }
-
-  render() {
-    const { data, myKey } = this.props;
-    return (
-      <li key={myKey} className={styles.item}>
-        <h3 className="">{data.name}</h3>
-        <div className="">Year: {data.birth_year}</div>
-        <div className="">Skin: {data.skin_color}</div>
-        <div className="">Height: {data.height}</div>
-        <div className="">Mass: {data.mass}</div>
-      </li>
-    );
-  }
+function Item({ data, myKey }: ItemProps) {
+  return (
+    <li key={myKey} className={styles.item}>
+      <h3 className="">{data.name}</h3>
+      <div className="">Year: {data.birth_year}</div>
+      <div className="">Skin: {data.skin_color}</div>
+      <div className="">Height: {data.height}</div>
+      <div className="">Mass: {data.mass}</div>
+    </li>
+  );
 }
 
 export default Item;
